5addd292ce7633f66a7c94d38eb80fdaf7ec2bc7
Ruth Deborah Juma <ruthdeborah@Ruths-MacBook-Pro.local>
2025-01-02 13:47:44 +0300

    Vercel config

--- smarthome_network_ids/wsgi.py
+++ smarthome_network_ids/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smarthome_network_ids.settings')
 
 application = get_wsgi_application()
+
+# api/settings.py
+WSGI_APPLICATION = 'api.wsgi.app'
